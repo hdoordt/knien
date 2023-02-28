91013903caf3f1fff5689ca04cf69e4e7ba75df2
Henk Oordt <hd@oordt.dev>
2023-02-28 20:21:19 +0100

    Initial setup of TopicBuilder

--- src/chan/topic.rs
+++ src/chan/topic.rs
@@ -1,4 +1,5 @@
 use std::marker::PhantomData;
+use std::str::Split;
 
 use async_trait::async_trait;
 use uuid::Uuid;
@@ -38,19 +39,18 @@ impl TopicChannel {
         })
     }
 
-    pub async fn consumer<B: TopicBus>(&self, consumer_tag: &str) -> Result<Consumer<Self, B>> {
-        let topic = todo!();
+    pub async fn consumer<B: TopicBus, T: AsRef<str>>(
+        &self,
+        routing_key: RoutingKey<T, B>,
+        consumer_tag: &str,
+    ) -> Result<Consumer<Self, B>> {
+        let topic = routing_key.repr.as_ref();
         self.inner
             .queue_declare(topic, Default::default(), Default::default())
             .await?;
         let consumer = self
             .inner
-            .basic_consume(
-                topic,
-                consumer_tag,
-                Default::default(),
-                Default::default(),
-            )
+            .basic_consume(topic, consumer_tag, Default::default(), Default::default())
             .await?;
 
         Ok(Consumer {
@@ -90,5 +90,126 @@ impl Channel for TopicChannel {
             .await?;
 
         Ok(())
+    }
+}
+
+
+pub struct RoutingKey<T, B> {
+    repr: T,
+    _marker: PhantomData<B>,
+}
+
+pub struct RoutingKeyBuilder<B> {
+    iter: Split<'static, &'static str>,
+    repr: String,
+    _marker: PhantomData<B>,
+}
+
+impl<B: TopicBus> RoutingKeyBuilder<B> {
+    /// Create a new [TopicBuilder]
+    pub fn new() -> Self {
+        let iter = B::TOPIC.split(".");
+        Self {
+            iter,
+            repr: String::new(),
+            _marker: PhantomData,
+        }
+    }
+
+    /// Add the next word to the routing key
+    pub fn word(self) -> Self {
+        let RoutingKeyBuilder {
+            mut iter,
+            mut repr,
+            _marker,
+        } = self;
+        if !repr.is_empty() {
+            repr.push('.');
+        }
+        repr.push_str(iter.next().expect("No more parts left in topic"));
+        Self {
+            iter,
+            repr,
+            _marker,
+        }
+    }
+
+    pub fn star(self) -> Self {
+        let RoutingKeyBuilder {
+            mut iter,
+            mut repr,
+            _marker,
+        } = self;
+
+        if !repr.is_empty() {
+            repr.push('.');
+        }
+        repr.push('*');
+        let _ = iter.next().expect("No more parts left in topic");
+        Self {
+            iter,
+            repr,
+            _marker,
+        }
+    }
+
+    pub fn hash(self) -> RoutingKey<String, B> {
+        let mut repr = self.repr;
+        if !repr.is_empty() {
+            repr.push('.');
+        }
+        repr.push('#');
+        RoutingKey {
+            repr,
+            _marker: PhantomData,
+        }
+    }
+
+    pub fn finish(self) -> RoutingKey<String, B> {
+        RoutingKey {
+            repr: self.repr,
+            _marker: PhantomData,
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        chan::topic::{RoutingKeyBuilder, TopicBus},
+        Bus,
+    };
+
+    #[test]
+    fn test_topic_builder() {
+        struct MyTopic;
+        impl Bus for MyTopic {
+            type PublishPayload = ();
+        }
+        impl TopicBus for MyTopic {
+            const TOPIC: &'static str = "this.is.a.cool.topic";
+        }
+
+        let builder: RoutingKeyBuilder<MyTopic> = RoutingKeyBuilder::new();
+        assert_eq!(builder.finish().repr, "");
+
+        let builder: RoutingKeyBuilder<MyTopic> = RoutingKeyBuilder::new();
+        assert_eq!(builder.word().finish().repr, "this");
+
+        let builder: RoutingKeyBuilder<MyTopic> = RoutingKeyBuilder::new();
+        assert_eq!(builder.word().star().finish().repr, "this.*");
+
+        let builder: RoutingKeyBuilder<MyTopic> = RoutingKeyBuilder::new();
+        assert_eq!(
+            builder.word().star().word().finish().repr,
+            "this.*.a"
+        );
+
+        let builder: RoutingKeyBuilder<MyTopic> = RoutingKeyBuilder::new();
+        assert_eq!(
+            builder.word().star().word().hash().repr,
+            "this.*.a.#"
+        );
     }
 }
